eaf26a3d43b9258e8f09e50bf28ab8d7a7c13044
Anthony Jhu <gledhilltom67@gmail.com>
2025-03-09 17:20:00 +0000

    Update prettier.config.js

--- app/prettier.config.js
+++ app/prettier.config.js
@@ -1,3 +1,14 @@
+The proposal/justification
+
+My solution for the client would be to create a website that provides the students with an easy to use place that helps them revise the work they have been doing with tutor and create an easy way for the tutor to track the progress of their students. This website will have a student or teacher option when signing up so that once the user is logged in the website can be more tailored to what the user would be using it for.
+
+One of the pages for this website would offer a wide variety of courses for the students and "provide access to digital content to encourage wider learning". These will help them go back over the topics they have been learning to retain and reinforce the information in their mind. This page would display all the courses on offer with a navigation bar to help find the course they want. These courses would display the information that the student has been learning about and then ask them basic questions about that information. E.g. for English asking them a multiple-choice question about what a certain word means. I've come up with this solution after looking at a website called Seneca that can been seen in my appendix or in my sources table. This is also what I am basing the look of my website on.
+
+Another page of this website that is only accessible by a tutor would allow them to "monitor the learner progress" their students are making on each course showing things like the speed they are getting through each course, how many questions they're getting right and an estimation of how well they seem to be comprehending the task. And after seeing this information the tutor can then leave comments for the students to see. Providing "interactive teaching" that can help the student in their weaker areas. The Tutor will also be able to set deadlines so that the student knows how long they have to complete it. I got the idea of tracking the student's data in each course from go student that can been seen in my appendix.
+
+To incorporate the features customers and tutors would want into the solution like "gamified learning" I would also make a page that allows tutors or students to create their own quizzes that students can do. These quizzes once done would show the score each student is getting on a leader board providing a competitive aspect to their learning and to try and make the quizzes seem more fun than just another bit of work to be done powerups can be added in that can provide students with different advantages during the quiz e.g. one powerup could remove some of the wrong answers so there's a better chance they get it right. I thought up this solution when looking at websites like kahoot and quizzz that can be seen in my appendix and in my sources table.
+
+
 // prettier.config.js or .prettierrc.js
 module.exports = {
     // 一行最多 100 字符
